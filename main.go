ba0e07f9c71d9d6f30dbb9eb207117d08d3ca225
thosapol <isocare.thospol@gmail.com>
2018-11-30 10:37:13 +0700

    Create Route CRUD of Users with mux

--- main.go
+++ main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func AllUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "AllUserEndPoint !")
+}
+
+func FindUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "FindUserEndpoint !")
+}
+
+func CreateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "CreateUserEndPoint !")
+}
+
+func UpdateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "UpdateUserEndPoint !")
+}
+
+func DeleteUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "DeleteUserEndPoint !")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/users", AllUserEndPoint).Methods("GET")
+	r.HandleFunc("/users", FindUserEndpoint).Methods("POST")
+	r.HandleFunc("/users", CreateUserEndPoint).Methods("PUT")
+	r.HandleFunc("/users", UpdateUserEndPoint).Methods("DELETE")
+	r.HandleFunc("/users/{id}", DeleteUserEndPoint).Methods("GET")
+	if err := http.ListenAndServe(":4525", r); err != nil {
+		log.Fatal(err)
+	}
+}
