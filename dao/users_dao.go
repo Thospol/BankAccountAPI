1d152fec58eef4f53d7a54d7393d26faa90f9bed
thosapol <isocare.thospol@gmail.com>
2018-11-30 14:45:52 +0700

    fix import and fmt.Printf user

--- dao/users_dao.go
+++ dao/users_dao.go
@@ -2,10 +2,11 @@ package dao
 
 import (
 	"bankaccountapi/model"
+	"fmt"
 	"log"
 
 	mgo "github.com/globalsign/mgo"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/globalsign/mgo/bson"
 )
 
 type UsersDAO struct {
@@ -30,26 +31,31 @@ func (m *UsersDAO) Connect() {
 func (u *UsersDAO) FindAll() ([]model.User, error) {
 	var users []model.User
 	err := db.C(COLLECTION).Find(bson.M{}).All(&users)
+	fmt.Printf("%#v\n", users)
 	return users, err
 }
 
 func (u *UsersDAO) FindById(id string) (model.User, error) {
 	var user model.User
 	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&user)
+	fmt.Printf("%#v\n", user)
 	return user, err
 }
 
 func (u *UsersDAO) Insert(user model.User) error {
 	err := db.C(COLLECTION).Insert(&user)
+	fmt.Printf("%#v\n", user)
 	return err
 }
 
 func (u *UsersDAO) Delete(user model.User) error {
 	err := db.C(COLLECTION).Remove(&user)
+	fmt.Printf("%#v\n", user)
 	return err
 }
 
 func (u *UsersDAO) Update(user model.User) error {
 	err := db.C(COLLECTION).UpdateId(user.ID, &user)
+	fmt.Printf("%#v\n", user)
 	return err
 }
