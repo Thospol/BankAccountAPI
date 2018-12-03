5ae2715b9f193375c1c8f0b0727ed89804516542
thosapol <isocare.thospol@gmail.com>
2018-12-03 12:22:54 +0700

    add route line /api/v1/users/{id}/bankAccount

--- route.go
+++ route.go
@@ -12,7 +12,7 @@ func InitRoute() {
 	r.HandleFunc("/api/v1/users", UpdateUserEndPoint).Methods("PUT")
 	r.HandleFunc("/api/v1/users", DeleteUserEndPoint).Methods("DELETE")
 	r.HandleFunc("/api/v1/users/{id}/bankAccount", CreateBankAccountOfUserEndPoint).Methods("POST")
-
+	r.HandleFunc("/api/v1/users/{id}/bankAccount", FindUserEndpoint).Methods("GET")
 	if err := http.ListenAndServe(":4525", r); err != nil {
 		log.Fatal(err)
 	}
